64abb673c90009f86c09ea444bfe8723a3f227a7
Suleiman Olaide <olaide.suleiman96@gmail.com>
2022-08-02 14:49:24 -0700

    Add and render navigation component

---
+++ src/components/nav.js
@@ -0,0 +1,14 @@
+/* eslint-disable no-unused-vars */
+import React from 'react';
+import { NavLink } from 'react-router-dom';
+import logo from './logo.png';
+
+const Nav = () => (
+  <div className="navbar">
+    <div>
+      <img src={logo} alt="logo" />
+    </div>
+  </div>
+);
+
+export default Nav;
