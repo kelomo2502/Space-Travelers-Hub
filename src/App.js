dd0b8479ca219da57572458224961a93bd10cb77
Oyewunmi Kelvin Gbenga <kelvinoye@gmail.com>
2022-08-02 16:56:33 +0100

    Add profile component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
 import './App.css';
+import { Routes, Route } from 'react-router-dom';
+import Profile from './components/profile';
 import Nav from './components/nav';
 
 function App() {
   return (
     <div>
       <Nav />
+      <Routes>
+        <Route path="/profile" element={<Profile />} />
+      </Routes>
     </div>
   );
 }
---
+++ src/components/profile.js
@@ -0,0 +1,6 @@
+const Profile = () => (
+  <div>
+    <h2>This is my profile</h2>
+  </div>
+);
+export default Profile;
