91986000a1efbdc72a0b47aa44cb690974eb814c
Oyewunmi Kelvin Gbenga <kelvinoye@gmail.com>
2022-08-03 13:49:16 +0100

    Add BrowserRouter to App.js

--- src/App.js
+++ src/App.js
@@ -1,19 +1,22 @@
 import './App.css';
-import { Routes, Route } from 'react-router-dom';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+
 import Profile from './components/profile';
 import Nav from './components/nav';
-import Rockets from './components/rockets';
+import Rockets from './components/Rockets';
 import Missions from './components/missions';
 
 function App() {
   return (
     <div className="page">
-      <Nav />
-      <Routes>
-        <Route path="/profile" element={<Profile />} />
-        <Route path="/rockets" element={<Rockets />} />
-        <Route path="/missions" element={<Missions />} />
-      </Routes>
+      <BrowserRouter>
+        <Nav />
+        <Routes>
+          <Route path="/profile" element={<Profile />} />
+          <Route path="/rockets" element={<Rockets />} />
+          <Route path="/missions" element={<Missions />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
